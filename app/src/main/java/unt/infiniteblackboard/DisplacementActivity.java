10c628bf2aacb55a6ad1eb5a55d83c95d5ec48d5
skmathew <skm20011@gmail.com>
2015-03-19 18:18:30 -0500

    text to speech for the content

--- app/src/main/java/unt/infiniteblackboard/DisplacementActivity.java
+++ app/src/main/java/unt/infiniteblackboard/DisplacementActivity.java
@@ -5,41 +5,67 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 
 public class DisplacementActivity extends ActionBarActivity {
 
-    private TextToSpeech tts;
+    TextToSpeech tts;
     ImageView imageView;
     private AnimationDrawable mAnim;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displacement);
+
+        TextView Textv1 = (TextView) findViewById(R.id.displacement1);
+        final String texts1 = Textv1.getText().toString();
+        TextView Textv2 = (TextView) findViewById(R.id.displacement2);
+        final String texts2 = Textv2.getText().toString();
 
 
 
         imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setBackgroundResource(R.drawable.view_animation);
-
-
         mAnim = (AnimationDrawable) imageView.getBackground();
+
+
+        //text to speech code
+        tts = new TextToSpeech(DisplacementActivity.this, new TextToSpeech.OnInitListener() {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    tts.setLanguage(Locale.US);
+
+                    tts.speak(texts1,TextToSpeech.QUEUE_ADD,null);
+                    tts.speak(texts2,TextToSpeech.QUEUE_ADD,null);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Feature not Supported in Your Device",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //text to speech code end
+
 
         final Button button = (Button) findViewById(R.id.Home_button);
         final Button button1 = (Button) findViewById(R.id.Previous_Button);
+
 
         // Capture button clicks
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
 
                 // Start NewActivity.class
-
-
                 Intent myIntent = new Intent(DisplacementActivity.this,
                         MainActivity.class);
                 startActivity(myIntent);
@@ -59,11 +85,6 @@ public class DisplacementActivity extends ActionBarActivity {
 
     }
 
-    private void speakWords(String question) {
-        //speak(question, TextToSpeech.QUEUE_ADD);
-        //speak straight away
-       
-    }
 
     @Override
     protected  void onPause() {
@@ -71,6 +92,14 @@ public class DisplacementActivity extends ActionBarActivity {
         if (mAnim.isRunning()){
             mAnim.stop();
         }
+
+        if(tts !=null)
+        {
+             tts.stop();
+             tts.shutdown();
+        }
+        super.onPause();
+
     }
 
     @Override
@@ -80,10 +109,18 @@ public class DisplacementActivity extends ActionBarActivity {
         if(hasFocused){
             mAnim.start();
         }
+
+
+
     }
 
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
 
 
