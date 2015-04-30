9cda171afa4421243cdcdbddf7037e399c2c58c2
skmathew <skm20011@gmail.com>
2015-04-30 15:22:58 -0500

    youtube API key

--- app/src/main/java/unt/infiniteblackboard/VelocityVideo.java
+++ app/src/main/java/unt/infiniteblackboard/VelocityVideo.java
@@ -4,15 +4,14 @@ package unt.infiniteblackboard;
  * Created by RAFI on 4/27/2015.
  */
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.MediaController;
-import android.widget.VideoView;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
 public class VelocityVideo extends ActionBarActivity {
@@ -27,7 +26,7 @@ public class VelocityVideo extends ActionBarActivity {
         vidView.setMediaController(vidControl);
 
         String vidAddress =
-                "http://r9---sn-aiglln7s.googlevideo.com/videoplayback?mt=1430298563&mv=m&ms=au&ip=2a02%3A2498%3Ae000%3A85%3A87%3A%3A2&ipbits=0&expire=1430320223&mm=31&id=o-ACQixO0b3ccPkL9yB_kyi57Mv5aT_S5PA_4pCR6FPMSg&mime=video%2Fmp4&pl=32&key=yt5&itag=18&signature=4C348F091F77BBA81E22EB55AFD11EDCC67A80C7.2297667BD46428F7EF055D4F1659B8000FB90A76&upn=mD_DcQyR36I&ratebypass=yes&source=youtube&nh=IgpwcjAzLmxocjE0KgkxMjcuMC4wLjE&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Cmime%2Cmm%2Cms%2Cmv%2Cnh%2Cpl%2Cratebypass%2Csource%2Cupn%2Cexpire&sver=3&fexp=900720%2C901802%2C907263%2C916943%2C934954%2C938028%2C9405997%2C9407123%2C9407663%2C9408050%2C9408619%2C9408708%2C9408710%2C9412517%2C9413150%2C947233%2C948124%2C952612%2C952637%2C952640%2C957201&initcwndbps=2357500&dur=705.236&title=Calculating+Average+Velocity+or+Speed";
+"http://r2---sn-bxpbiucg51pa-btoe.googlevideo.com/videoplayback?itag=22&pcm2cms=yes&signature=2EF217AA1C001AA3E6D35A4E8F34C8AB2AD60406.DC2434FE832487393EB7D24925A11E1981994A5E&expire=1430445010&mm=31&ip=177.52.66.113&key=yt5&ms=au&mv=m&mt=1430423214&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Cmime%2Cmm%2Cms%2Cmv%2Cpcm2cms%2Cpl%2Cratebypass%2Csource%2Cupn%2Cexpire&pl=24&id=o-AJTV_mEPWXCBlOwdIoKCHfFJ7Hm7ZkMgw6eOF_XYXlbu&mime=video%2Fmp4&sver=3&ratebypass=yes&upn=MKx3g_a1U0g&ipbits=0&fexp=900720%2C907263%2C916656%2C934954%2C937420%2C938028%2C9405994%2C9406813%2C9407000%2C9407664%2C9407882%2C9408137%2C9408142%2C9408706%2C9409252%2C9412761%2C948124%2C948825%2C952612%2C952637%2C955202%2C957201&initcwndbps=332500&source=youtube&dur=705.236&title=Calculating+Average+Velocity+or+Speed";
         Uri vidUri = Uri.parse(vidAddress);
         vidView.setVideoURI(vidUri);
         vidView.start();
