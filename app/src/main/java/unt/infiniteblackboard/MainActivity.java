e1ecc4e409916aa2a8a69e46b288d056eb0fe93f
Alberto <converseguy923@gmail.com>
2015-05-04 13:29:17 -0500

    Changes to main to delete the random input of lines

--- app/src/main/java/unt/infiniteblackboard/MainActivity.java
+++ app/src/main/java/unt/infiniteblackboard/MainActivity.java
@@ -76,7 +76,6 @@ public class MainActivity extends ActionBarActivity {
                         startActivity(intent);
                         break;
 
-<<<<<<< HEAD
 /*                    case 3:
                         intent = new Intent(MainActivity.this, AverageVelocityActivity.class);
                         startActivity(intent);
@@ -97,10 +96,6 @@ public class MainActivity extends ActionBarActivity {
                         intent = new Intent(MainActivity.this, SpeedActivity.class);
                         startActivity(intent);
                         break;
-=======
-                    case 3:
-                        intent = new Intent(MainActivity.this, AverageVelocityActivity)
->>>>>>> 9d035ba2e5509fb243118775359a3b2b125a5ef7
 
                     case 7:
                         intent = new Intent(MainActivity.this, VideoActivity.class);
